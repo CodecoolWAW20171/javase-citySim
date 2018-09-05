a0ea48a4aa62047947f96ecb15f10ccf8fb8c114
matixk <matixk73@gmail.com>
2018-09-05 11:38:42 +0200

    Simple road added

--- src/com/codecool/citySim/controller/CitySimPaneController.java
+++ src/com/codecool/citySim/controller/CitySimPaneController.java
@@ -1,10 +1,24 @@
 package com.codecool.citySim.controller;
 
+import com.codecool.citySim.model.roads.Road;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.layout.Pane;
 
-public class CitySimPaneController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CitySimPaneController implements Initializable {
     @FXML
     public Pane pane;
+    public Road road;
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        road.setStartX(0);
+        road.setStartY(360);
+        road.setEndX(1280);
+        road.setEndY(360);
+        road.prefWidth(150);
+    }
 }
