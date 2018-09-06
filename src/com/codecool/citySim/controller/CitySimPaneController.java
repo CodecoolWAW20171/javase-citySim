cb73a184a158ab0a5498ef8a3943ed3e8b906c66
matixk <matixk73@gmail.com>
2018-09-06 11:20:17 +0200

    Quadric curve added

--- src/com/codecool/citySim/controller/CitySimPaneController.java
+++ src/com/codecool/citySim/controller/CitySimPaneController.java
@@ -5,6 +5,7 @@ import com.codecool.citySim.model.roads.Road;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.QuadCurve;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,13 +17,14 @@ public class CitySimPaneController implements Initializable {
     public Road horizontalRight;
     public Road verticalUp;
     public Road verticalDown;
+    public QuadCurve kurva;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        Car car = new Car(20, -360);
-        Car car1 = new Car(20, -300);
-        Car car2 = new Car(20, -240);
-        Car car3 = new Car(20, -180);
+        Car car = new Car(3, -360);
+        Car car1 = new Car(-13, -360);
+        Car car2 = new Car(3, -240);
+        Car car3 = new Car(-13, -240);
         pane.getChildren().add(car);
         pane.getChildren().add(car1);
         pane.getChildren().add(car2);
