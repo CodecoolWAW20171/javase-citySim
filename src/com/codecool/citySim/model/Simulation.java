b686b576b26055970cdaf863241449ce4f88ad47
matixk <matixk73@gmail.com>
2018-09-17 14:53:38 +0200

    Roads moved to Simulation

---
+++ src/com/codecool/citySim/model/Simulation.java
@@ -0,0 +1,26 @@
+package com.codecool.citySim.model;
+
+import com.codecool.citySim.model.roads.Road;
+
+public class Simulation {
+    private Road[] firstRoads = {
+            new Road(-640, 8, -16, 8),
+            new Road(640, -8, -8, -8),
+            new Road(-8, -360, -8, -16),
+            new Road(8, 360, 8, 16)
+    };
+    private Road[] secondRoads = {
+            new Road(16, 8, 640, 8),
+            new Road(-16, -8, -640, -8),
+            new Road(-8, 16, -8, 360),
+            new Road(8, 16, 8, -360)
+    };
+
+    public Road[] getFirstRoads() {
+        return firstRoads;
+    }
+
+    public Road[] getSecondRoads() {
+        return secondRoads;
+    }
+}
