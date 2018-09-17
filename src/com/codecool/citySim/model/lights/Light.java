30641f66964f2f69dca6c333ef467190749bc26c
grzban <grzegorz.banaszek@gmail.com>
2018-09-17 12:02:35 +0200

    Implemented lights using threads

--- src/com/codecool/citySim/model/lights/Light.java
+++ src/com/codecool/citySim/model/lights/Light.java
@@ -1,7 +1,62 @@
-package com.codecool.citySim.model;
+package com.codecool.citySim.model.lights;
 
-import java.awt.*;
+public class Light implements Runnable {
 
-public class Light {
-    Color state;
+    private boolean isGreenHorizontal;
+    private boolean isGreenVertical;
+    private long timeOfLight;
+
+    public Light() {
+        isGreenHorizontal = false;
+        isGreenVertical = true;
+        setTimeOfLight(5000);
+    }
+
+    @Override
+    public void run() {
+        System.out.println(toString());
+        while (true) {
+            setGreenVertical(!isGreenVertical());
+            setGreenHorizontal(!isGreenHorizontal());
+            System.out.println(toString());
+            try {
+                Thread.sleep(getTimeOfLight());
+            } catch (InterruptedException e) {
+                return;
+            }
+        }
+    }
+
+    public long getTimeOfLight() {
+        return timeOfLight;
+    }
+
+    public void setTimeOfLight(long timeOfLight) {
+        this.timeOfLight = timeOfLight;
+    }
+
+    public boolean isGreenHorizontal() {
+        return isGreenHorizontal;
+    }
+
+    public void setGreenHorizontal(boolean greenHorizontal) {
+        isGreenHorizontal = greenHorizontal;
+    }
+
+    public boolean isGreenVertical() {
+        return isGreenVertical;
+    }
+
+    public void setGreenVertical(boolean greenVertical) {
+        isGreenVertical = greenVertical;
+    }
+
+    @Override
+    public String toString() {
+        return "Light{" +
+                "isGreenHorizontal=" + isGreenHorizontal() +
+                ", isGreenVertical=" + isGreenVertical() +
+                ", timeOfLight=" + getTimeOfLight() +
+                '}';
+    }
 }
--- src/com/codecool/citySim/model/roads/Crossroad.java
+++ src/com/codecool/citySim/model/roads/Crossroad.java
@@ -1,6 +1,6 @@
 package com.codecool.citySim.model.roads;
 
-import com.codecool.citySim.model.Light;
+import com.codecool.citySim.model.lights.Light;
 
 public class Crossroad {
     Road[] road = new Road[12];
