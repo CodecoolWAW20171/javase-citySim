a76e48b285447cb479e01bdf7dbc75475ccc754a
matixk <matixk73@gmail.com>
2018-09-17 14:36:59 +0200

    Road to model

--- src/com/codecool/citySim/model/roads/Road.java
+++ src/com/codecool/citySim/model/roads/Road.java
@@ -5,6 +5,29 @@ import javafx.scene.shape.Line;
 
 import java.util.LinkedList;
 
-public class Road extends Line {
+public class Road {
+    private Double[] startPos;
+    private Double[] endPos;
     private LinkedList<Vehicle> vehicles;
+
+    public Road(double startX, double startY, double endX, double endY) {
+        startPos = new Double[]{startX, startY};
+        endPos = new Double[]{endX, endY};
+    }
+
+    public double getStartX() {
+        return startPos[0];
+    }
+
+    public double getStartY() {
+        return startPos[1];
+    }
+
+    public double getEndX() {
+        return endPos[0];
+    }
+
+    public double getEndY() {
+        return endPos[1];
+    }
 }
